32184fba067a56ef16de0d168d0d6536833a6c38
aituar17 <159922336+aituar17@users.noreply.github.com>
2025-07-14 15:52:20 +0300

    Update joint_rpca.R

--- R/joint_rpca.R
+++ R/joint_rpca.R
@@ -1,25 +1,3 @@
-#load all necessary packages
-suppressPackageStartupMessages({
-library(Matrix)
-library(irlba)
-library(RSpectra)
-library(pracma)
-library(optimx)
-library(glmnet)
-library(caret)
-library(dplyr)
-library(tidyr)
-library(ggplot2)
-library(plotly)
-library(factoextra)
-library(vegan)
-library(randomForest)
-library(ROCR)
-library(vegan)
-library(ggforce)
-library(concaveman)
-})
-
 #FUNCTION FOR JOINT RPCA
 
 #' Joint Robust PCA on Multiple Compositional Tables
